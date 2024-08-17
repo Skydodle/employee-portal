71e8e955cd9609ca6b5cf5d36fbf81d9d330e483
Johnny Wu <skydodle@gmail.com>
2024-08-17 11:55:50 -0700

    Set up basic login component to test redux state

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,27 @@
-import { Button, Typography } from "@mui/material";
-import "./App.css";
+/** @format */
+
+import { Button, Typography } from '@mui/material';
+import './App.css';
+import Login from './components/Login/Login';
 
 function App() {
   return (
     <>
-      <Typography variant="h1" component="h1" sx={{ mb: 2 }}>
-        Material UI Vite.js example
-      </Typography>
-      <Button color="primary">primary</Button>
-      <Button color="secondary">secondary</Button>
-      <Button color="error">error</Button>
+      <div>
+        <Typography
+          variant='h1'
+          component='h1'
+          sx={{ mb: 2 }}
+        >
+          Material UI Vite.js example
+        </Typography>
+        <Button color='primary'>primary</Button>
+        <Button color='secondary'>secondary</Button>
+        <Button color='error'>error</Button>
+      </div>
+      <div>
+        <Login />
+      </div>
     </>
   );
 }
