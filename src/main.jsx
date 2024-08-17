00ab32083712afa9da9fafd713aaae3aa1dc1b90
Johnny Wu <skydodle@gmail.com>
2024-08-17 11:54:40 -0700

    Set up Redux Provider wrapper

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,27 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
-import CssBaseline from "@mui/material/CssBaseline";
-import { ThemeProvider } from "@mui/material/styles";
-import theme from "./theme";
+/** @format */
 
-createRoot(document.getElementById("root")).render(
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+
+import CssBaseline from '@mui/material/CssBaseline';
+import { ThemeProvider } from '@mui/material/styles';
+import theme from './theme';
+
+import { Provider } from 'react-redux';
+import store from './store/store.js';
+
+import App from './App.jsx';
+
+createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ThemeProvider theme={theme}>
-      {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-      <CssBaseline />
-      <App />
-    </ThemeProvider>
+    <Provider store={store}>
+      {' '}
+      {/* Wrap the app with Provider to pass the store */}
+      <ThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
+    </Provider>
   </StrictMode>
 );
