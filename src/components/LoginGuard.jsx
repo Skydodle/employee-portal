11e530d6a42c051c7e161da95b501ac5818d0f95
Johnny Wu <skydodle@gmail.com>
2024-08-19 12:25:26 -0700

    Update to LoginGuard to separate from RegistrationGuard

--- src/components/LoginGuard.jsx
+++ src/components/LoginGuard.jsx
@@ -1,10 +1,12 @@
+/** @format */
+
 import { Outlet } from 'react-router-dom';
 
-const Guard = () => {
+const LoginGuard = () => {
   const isLoggedIn = true;
 
   // // conditional rendering
   return isLoggedIn ? <Outlet /> : <h1>You are not logged in</h1>;
 };
 
-export default Guard;
+export default LoginGuard;
