b426eab4c5dd2016858c68dda47ef058e8616136
Johnny Wu <skydodle@gmail.com>
2024-08-22 14:33:21 -0700

    Added catchall route to redirect to login

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 /** @format */
 
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import {
   Register,
   Login,
@@ -8,27 +8,30 @@ import {
   Profile,
   VisaStatus,
   Housing,
-  Report,
-} from "./pages";
-import LoginGuard from "./components/LoginGuard";
-import RegisterGuard from "./components/RegisterGuard";
-import OnboardingTesting from "./pages/OnboardingTesting";
+  Report
+} from './pages';
+import LoginGuard from './components/LoginGuard';
+import RegisterGuard from './components/RegisterGuard';
+import OnboardingTesting from './pages/OnboardingTesting';
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route element={<RegisterGuard />}>
-          <Route path="/register" element={<Register />} />
+          <Route path='/register' element={<Register />} />
         </Route>
-        <Route path="/login" element={<Login />} />
+        <Route path='/login' element={<Login />} />
         <Route element={<LoginGuard />}>
-          <Route path="/onboarding" element={<Onboarding />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/visa-status" element={<VisaStatus />} />
-          <Route path="/housing" element={<Housing />} />
-          <Route path="/housing/report" element={<Report />} />
-          <Route path="/testing" element={<OnboardingTesting />} />
+          <Route path='/onboarding' element={<Onboarding />} />
+          <Route path='/profile' element={<Profile />} />
+          <Route path='/visa-status' element={<VisaStatus />} />
+          <Route path='/housing' element={<Housing />} />
+          <Route path='/housing/report' element={<Report />} />
+          <Route path='/testing' element={<OnboardingTesting />} />
         </Route>
+
+        {/* Catch-all route that redirects base URL to /login */}
+        <Route path='/' element={<Navigate to='/login' />} />
       </Routes>
     </BrowserRouter>
   );
