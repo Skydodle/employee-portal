b029ef650e98b7e9cbe6e9b4c6fb30b7fdff76dd
Johnny Wu <skydodle@gmail.com>
2024-08-22 14:34:29 -0700

    Add reusable LogoutButton and replace button in stepper

--- src/components/OnBoardingStepper.jsx
+++ src/components/OnBoardingStepper.jsx
@@ -1,14 +1,17 @@
-import PropTypes from "prop-types";
-import Box from "@mui/material/Box";
-import Typography from "@mui/material/Typography";
-import { Stepper, Step, StepButton, Button, StepLabel } from "@mui/material";
-import { STATUS } from "../constants/onBoarding";
+/** @format */
+
+import PropTypes from 'prop-types';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import { Stepper, Step, StepButton, Button, StepLabel } from '@mui/material';
+import { STATUS } from '../constants/onBoarding';
+import LogoutButton from './LogoutButton';
 function OnBoardingStepper({
   activeStep,
   setActiveStep,
   sections,
   errors = [],
-  completed = [],
+  completed = []
 }) {
   const handleStep = (step) => () => {
     if (step < activeStep) {
@@ -17,18 +20,19 @@ function OnBoardingStepper({
   };
   return (
     <>
-      <Stepper nonLinear activeStep={activeStep} orientation="vertical">
+      <Stepper nonLinear activeStep={activeStep} orientation='vertical'>
         {sections.map((section, index) => (
           <Step key={section} completed={completed[index]}>
-            <StepButton color="inhererit" onClick={handleStep(index)}>
+            <StepButton color='inhererit' onClick={handleStep(index)}>
               <StepLabel error={errors[index]}> {section}</StepLabel>
             </StepButton>
           </Step>
         ))}
       </Stepper>
-      <Button variant="text" color="secondary" sx={{ marginTop: 10 }}>
+      {/* <Button variant='text' color='secondary' sx={{ marginTop: 10 }}>
         Log Out
-      </Button>
+      </Button> */}
+      <LogoutButton sx={{ marginTop: 10 }} />
     </>
   );
 }
@@ -38,7 +42,7 @@ OnBoardingStepper.propTypes = {
   setActiveStep: PropTypes.func,
   sections: PropTypes.arrayOf(PropTypes.string),
   errors: PropTypes.arrayOf(PropTypes.bool),
-  completed: PropTypes.arrayOf(PropTypes.bool),
+  completed: PropTypes.arrayOf(PropTypes.bool)
 };
 
 export default OnBoardingStepper;
