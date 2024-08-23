ea5ff6335c0949041ccce835fae1bda272f01ea0
Gavin-Yeah <gavinjiadaye@gmail.com>
2024-08-23 15:44:02 -0400

    PORTAL-56 refactor logout page

--- src/pages/Logout.jsx
+++ src/pages/Logout.jsx
@@ -1,17 +1,13 @@
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { logout } from "../store/userSlice/user.slice";
-import { useNavigate } from "react-router-dom";
-function Logout3(props) {
+import { Navigate } from "react-router-dom";
+function Logout3() {
   const dispatch = useDispatch();
-  const navigate = useNavigate();
   useEffect(() => {
     dispatch(logout());
-    navigate("/login");
-  }, []);
-  return <div>Logout3</div>;
+  }, [dispatch]);
+  return <Navigate to="/login" />;
 }
-
-Logout3.propTypes = {};
 
 export default Logout3;
